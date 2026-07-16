9c790d4d50bf0922d377ff4f82f342aac45c38f0
Stephanie VerSteeg <shigginsdev@gmail.com>
2026-07-16 16:13:46 -0600

    adding Cognito sign in sign up

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,18 @@ import { createApp } from 'vue'
 
 import App from './App.vue'
 import router from './router'
+import { Amplify } from 'aws-amplify'
+
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolId: 'us-east-2_LRPKdckjF',
+      userPoolClientId: '70mjg0ubnslps0057vp3d74rru',
+      loginWith: {
+        email: true,
+      },
+    },
+  },
+})
 
 createApp(App).use(router).mount('#app')
